122e544d57ef5357803367f2087407fb78986d92
Marinko Mišković <marinko.miskovic.hr@gmail.com>
2020-06-25 18:09:05 +0200

    Fixing bug

--- src/js/helpers/global-variables.js
+++ src/js/helpers/global-variables.js
@@ -1,5 +1,5 @@
 const weather_api_base_url = "https://api.openweathermap.org/data/2.5/weather";
-const weather_api_key = "8c0652b8fc6ac5d41bf19358d5f0873a";
+const weather_api_key = "";
 
 const oslo_id = "3143244";
 const los_angeles_id = "5368361";
